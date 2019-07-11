d7e256e875209179fed74ec351e1197cd581caaa
Hugo Zammit <hugo.zammit@signature-code.com>
2019-07-11 17:09:33 +0200

    Add MutateNodeWithCount.

--- AbstractSyntaxTree.Visitor/OptimizationMutator.cs
+++ AbstractSyntaxTree.Visitor/OptimizationMutator.cs
@@ -7,28 +7,57 @@ namespace AbstractSyntaxTree.Visitor
 {
     public class OptimizationMutator : NodeMutator
     {
+        private int _count;
+
+        public (Node Node, int Count) MutateNodeWithCount( Node node )
+        {
+            var mutatedNode = MutateNode( node );
+            return (mutatedNode, _count);
+        }
+
+        public override Node Mutate( ConstantNode node )
+        {
+            _count = 1;
+            return node;
+        }
+
+        public override Node Mutate( ErrorNode node )
+        {
+            _count = 1;
+            return node;
+        }
+
+        public override Node Mutate( IdentifierNode node )
+        {
+            _count = 1;
+            return node;
+        }
+
         public override Node Mutate( UnaryNode node )
         {
             Debug.Assert( node.Type == TokenType.Minus );
 
             var mutated = MutateNode( node.Operand );
             if( mutated is ConstantNode constant )
-            {
                 return new ConstantNode( -constant.Value );
-            }
 
+            _count += 1;
             return mutated != node ? mutated : node;
         }
 
         public override Node Mutate( BinaryNode node )
         {
             var leftMutated = MutateNode( node.Left );
+            var leftCount = _count;
+
             var rightMutated = MutateNode( node.Right );
+            _count = leftCount + _count + 1;
 
             var leftConstant = leftMutated as ConstantNode;
             var rightConstant = rightMutated as ConstantNode;
             if( leftConstant != null && rightConstant != null )
             {
+                _count = 1;
                 switch( node.Type )
                 {
                     case TokenType.Plus:           return new ConstantNode( leftConstant.Value + rightConstant.Value );
@@ -39,29 +68,50 @@ namespace AbstractSyntaxTree.Visitor
                 }
             }
 
-            if( node.Type == TokenType.Minus )
-                if( rightMutated is UnaryNode rightUnary )
+            switch( node.Type ) {
+                case TokenType.Minus when rightMutated is UnaryNode rightUnary:
+                    _count -= 1;
                     return new BinaryNode( TokenType.Plus, leftMutated, rightUnary.Operand );
-                else if( rightConstant != null )
+
+                case TokenType.Minus when rightConstant != null:
                     return new BinaryNode( TokenType.Plus, leftMutated, new ConstantNode( -rightConstant.Value ) );
 
-            if( node.Type == TokenType.Divide && leftMutated.ToString() == rightMutated.ToString() )
-                return new ConstantNode( 1.0 );
+                case TokenType.Divide when leftMutated.ToString() == rightMutated.ToString():
+                    _count = 1;
+                    return new ConstantNode( 1.0 );
 
-            return leftMutated == node.Left && rightMutated == node.Right
-                ? node
-                : new BinaryNode( node.Type, leftMutated, rightMutated );
+                default:
+                    return leftMutated == node.Left && rightMutated == node.Right
+                        ? node
+                        : new BinaryNode( node.Type, leftMutated, rightMutated );
+            }
         }
 
         public override Node Mutate( TernaryNode node )
         {
             var conditionMutated = MutateNode( node.Condition );
+            var conditionCount = _count;
+
             var whenTrueMutated = MutateNode( node.WhenTrue );
-            var whenFalseMutated = node.WhenFalse != null ? MutateNode( node.WhenFalse ) : null;
+            var whenTrueCount = _count;
+
+            Node whenFalseMutated = null;
+            if( node.WhenFalse != null )
+                whenFalseMutated = MutateNode( node.WhenFalse );
+            else
+                _count = 0;
 
             if( conditionMutated is ConstantNode conditionConstant )
-                return conditionConstant.Value > 0 ? whenTrueMutated : whenFalseMutated;
+            {
+                if( !(conditionConstant.Value > 0) )
+                    return whenFalseMutated;
 
+                _count = whenTrueCount;
+                return whenTrueMutated;
+
+            }
+
+            _count = conditionCount + whenTrueCount + _count + 1;
             return conditionMutated == node.Condition
                 && whenTrueMutated == node.WhenTrue
                 && whenFalseMutated == node.WhenFalse
--- Tests/AbstractSyntaxTree.Genetic.Tests/OptimizationMutatorTest.cs
+++ Tests/AbstractSyntaxTree.Genetic.Tests/OptimizationMutatorTest.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using AbstractSyntaxTree.Analyzer;
+using AbstractSyntaxTree.Model;
+using AbstractSyntaxTree.Model.Abstraction;
 using AbstractSyntaxTree.Visitor;
 using FluentAssertions;
 using NUnit.Framework;
@@ -14,12 +17,28 @@ namespace AbstractSyntaxTree.Genetic.Tests
         public void optimizer_in_action( string input, string optimized )
         {
             var node = new NodeAnalyzer().Parse( input );
-
             var optimizedNode = new OptimizationMutator().MutateNode( node );
-
             var printer = new PrettyPrintVisitor();
             printer.VisitNode( optimizedNode );
             printer.ToString().Should().Be( optimized );
+        }
+
+        [TestCase( "1", 1 )]
+        [TestCase( "x", 1 )]
+        [TestCase( "¤", 1 )]
+        [TestCase( "-x", 2 )]
+        [TestCase( "-1", 1 )]
+        [TestCase( "x + y", 3 )]
+        [TestCase( "1 + 1 ", 1 )]
+        [TestCase( "x / x ", 1 )]
+        [TestCase( "x ? 1 : 0", 4 )]
+        [TestCase( "1 ? x : y", 1 )]
+        [TestCase( "0 ? x : y", 1 )]
+        public void MutateNodeWithCount( string expression, int expectedCount )
+        {
+            var node = new NodeAnalyzer().Parse( expression );
+            var optimizedNode = new OptimizationMutator().MutateNodeWithCount( node );
+            optimizedNode.Count.Should().Be( expectedCount );
         }
     }
 }
